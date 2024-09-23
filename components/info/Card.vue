d21e057b01a5b5f1441e7a78256bf8e516cc46d2
Sam Chiu <samchiu064@gmail.com>
2024-09-23 21:08:01 +0800

    feat(info): 新增 Info 元件

---
+++ components/info/Card.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = defineProps<{
+  type?: 'warning' | 'info' | 'info-purple'
+  title?: string
+  number?: number
+  message?: string
+}>()
+
+const backgroundClass = computed(() => {
+  switch (props.type) {
+    case 'warning':
+      return 'bg-[#FFEFE7]'
+    case 'info':
+      return 'bg-[#E8F0FB]'
+    case 'info-purple':
+      return 'bg-[#FDEBF9]'
+    default:
+      return ''
+  }
+})
+
+const messageClass = computed(() => {
+  switch (props.type) {
+    case 'warning':
+      return 'text-primary-red'
+    case 'info':
+      return 'text-[#3786f1]'
+    case 'info-purple':
+      return 'text-[#EE61CF]'
+    default:
+      return ''
+  }
+})
+</script>
+
+<template>
+  <base-card :class="backgroundClass" :border="false">
+    <template #title> {{ props.title }} </template>
+    <template #description>
+      <div class="flex flex-col gap-y-2.5">
+        <span class="text-4xl text-tertiary font-medium">{{
+          props.number
+        }}</span>
+        <span :class="['text-base', messageClass]">{{ props.message }}</span>
+      </div>
+    </template>
+  </base-card>
+</template>
---
+++ components/info/CardChart.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+const props = defineProps<{
+  title?: string
+  number?: number
+  men?: number
+  women?: number
+  image?: string
+}>()
+</script>
+
+<template>
+  <base-card class="h-[190px] relative min-w-[178px]" :border="true" size="md">
+    <template #title> {{ props.title }}</template>
+    <template #description>
+      <div class="flex flex-col h-full">
+        <span class="text-5xl text-tertiary font-medium mt-3">
+          {{ props.number }}</span
+        >
+        <div class="flex flex-col mt-auto">
+          <span class="text-sm text-primary-subtitle"
+            >{{ props.men }} 男性</span
+          >
+          <span class="text-sm text-primary-subtitle"
+            >{{ props.women }} 女性</span
+          >
+        </div>
+      </div>
+      <img
+        class="absolute top-1/2 right-0 transform -translate-y-1/2 -translate-x-5 z-[-1]"
+        :src="props.image"
+        alt="績效圖表"
+      />
+    </template>
+  </base-card>
+</template>
